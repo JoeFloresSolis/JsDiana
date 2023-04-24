096b0d7b31f291fd67090747b6246cb70b4bb385
Diana <anaidlizana8@gmail.com>
2023-04-24 16:51:45 -0500

    numero Mayor

---
+++ Funciones/NumeroMayor.js
@@ -0,0 +1,22 @@
+/*
+function NumeroMayor(a,b,c){
+          if (a>b && a>c){
+            return a;
+          }else if (b>a && b>c){
+            return b;
+          }else
+            return c;
+          }
+
+          console.log("Numero Mayor es:" + NumeroMayor(1,2,3));
+
+*/
+function NumeroMayor(a,b,c){
+    var Mayor=a;
+    if (b>Mayor){
+      return Mayor=b;
+    }else if(c>Mayor){
+      return Mayor=c;
+    }
+}
+    console.log("Numero Mayor es:" + NumeroMayor(1,4,3));
